65f87d327aefc5db6679d9cd55f089347c002536
Alumno420-BI <118073533+Alumno420@users.noreply.github.com>
2023-07-07 10:35:19 +0200

    Add files via upload

--- actions/actions.py
+++ actions/actions.py
@@ -24,14 +24,14 @@ class ActionDefaultFallback(Action):
     
             message = "Disculpe, no le he entendido."
             dispatcher.utter_message(text=message)
-            # tracker pausado y se deshace la última interacción del usuario
-            return [ConversationPaused(), UserUtteranceReverted()]
+            # se deshace la última interacción del usuario
+            return [UserUtteranceReverted()]
 
-class ValidateSimplePizzaForm(FormValidationAction):
+class ValidateAsignaturaTipoDatoForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_asignatura_tipo_dato_form"
 
-    def validate_subject(
+    def validate_asignatura(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
@@ -39,14 +39,19 @@ class ValidateSimplePizzaForm(FormValidationAction):
         domain: DomainDict,
     ) -> Dict[Text, Any]:
         # Validar si la asignatura pedida se encuentra tratada
+        subjects = []
+        for i in range(len(ALLOWED_SUBJECTS)):
+                if i % 2 != 0:
+                    subjects.append(ALLOWED_SUBJECTS[i])
+        subjects = ', '.join(subjects)
         if slot_value.lower() not in ALLOWED_SUBJECTS:
-            subjects = ', '.join(ALLOWED_SUBJECTS[:-1]) + ' y ' + ALLOWED_SUBJECTS[-1]
+        
             dispatcher.utter_message(text=f"De momento solo puedo informarte sobre las siguientes asignaturas: {subjects}")
             return {"asignatura": None}
         dispatcher.utter_message(text=f"OK! Entonces te interesa la materia {slot_value}.")
         return {"asignatura": slot_value}
 
-    def validate_data_type(
+    def validate_tipo_dato(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
@@ -54,10 +59,19 @@ class ValidateSimplePizzaForm(FormValidationAction):
         domain: DomainDict,
     ) -> Dict[Text, Any]:
         # Validar el tipo de dato
-
-        if slot_value.lower() not in ALLOWED_DATA_TYPES:
-            dispatcher.utter_message(text=f"I don't recognize that pizza. We serve {'/'.join(ALLOWED_PIZZA_TYPES)}.")
-            return {"tipo_dato": None}
         slot_asignatura = tracker.get_slot("asignatura")
+        
+        if slot_asignatura == None and slot_value.lower() not in ALLOWED_DATA_TYPES:
+            dispatcher.utter_message(text=f"No tengo {slot_value.lower()} como tipo de dato, tengo datos de {','.join(ALLOWED_DATA_TYPES)}. Primero necesito que me digas la asignatura que te interesa.")
+            return {"tipo_dato": None}
+
+        if slot_value.lower() not in ALLOWED_DATA_TYPES and slot_asignatura != None:
+            dispatcher.utter_message(text=f"No tengo {slot_value.lower()} como tipo de dato de {slot_asignatura}, tengo datos de {','.join(ALLOWED_DATA_TYPES)}.")
+            return {"tipo_dato": None}
+        
+        if slot_asignatura == None and slot_value.lower() in ALLOWED_DATA_TYPES:
+            dispatcher.utter_message(text=f"Tengo {slot_value.lower()} como tipo de dato, pero primero necesito que me digas la asignatura que te interesa.")
+            return {"tipo_dato": slot_value}
+
         dispatcher.utter_message(text=f"OK! Le voy a informar sobre {slot_value} de {slot_asignatura}.")
         return {"tipo_dato": slot_value}
