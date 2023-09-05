f58c31d24d84f77bac8d8a7ea29280f657b00f1d
Alumno-BI/STEMBach <118073533+Alumno420@users.noreply.github.com>
2023-09-05 18:02:29 +0200

    Mirar el CHANGELOG.md para más info

--- actions/actions.py
+++ actions/actions.py
@@ -16,28 +16,59 @@ from rasa_sdk.events import SlotSet
 # Para eliminar las tildes
 import unidecode
 
-# Para poder tratar el .xlsx que funciona como DB
-import pandas as pd
+# Para poder tratar el .sqlite que funciona como DB
+import sqlite3
 
 # clase para obtener los datos del documento .xlsx con respuestas
 class manager_dataDB:
-    def __init__(self, name: str):
-        # name tiene que ser el path a un archivo .xlsx, 
-        # no se usó .csv por su falta da soporte por defecto de Unicode
-        self.path = name
-        self.df = pd.read_excel(self.path)
-        self.diccionario = {}
+    def __init__(self, table: str, db: str):
         
-        for columna in self.df.columns:
-            valores = self.df[columna].tolist()
-            self.diccionario[columna] = valores
-    
+        self.table = table
+        self.db = db
+        self.nombre_columnas = []
+        self.data_dict = {}
+        # Establecer una conexión a la base de datos SQLite
+        try:
+            self.conn = sqlite3.connect(self.db)  # Reemplaza 'data.sqlite' con el nombre de tu archivo SQLite
+        except sqlite3.Error as e:
+            print("Error al conectar SQLite3:", e)
+            exit(1)
+        
+        self.cursor = self.conn.cursor()
+
+        # Obtener los nombres de las columnas de la tabla
+        consulta_columnas = f"PRAGMA table_info({self.table});"
+
+        try:
+            self.cursor.execute(consulta_columnas)
+            columnas = self.cursor.fetchall()
+        except sqlite3.Error as e:
+            print("Error al obtener las columnas:", e)
+            self.conn.close()
+            exit(1)
+        
+        for columna in columnas:
+            self.nombre_columnas.append(columna[1])
+
+            consulta_valores = f"SELECT \"{columna[1]}\" FROM data;"  # Reemplaza 'columna2' y 'nombre_tabla' según corresponda
+            try:
+                self.cursor.execute(consulta_valores)
+                valores = self.cursor.fetchall()
+                self.data_dict[columna[1]] = list(valores)
+            except sqlite3.Error as e:
+                print(f"Error al acceder la tabla {self.table} en 3 {self.db} :", e)
+                self.conn.close()
+                exit(1)
+        
+        self.conn.close()
+
     def obtener_index_columna(self, clave):
         if isinstance(clave, int):
             return None
         
         else:
-            for i in self.diccionario.keys():
+            for i in self.data_dict.keys():
+
                 if i == clave:
                     index = i
                     return index
@@ -49,49 +80,48 @@ class manager_dataDB:
             return clave
         
         else:
-            for i in range(len(self.diccionario[self.df.columns[0]])):
-                if self.diccionario[self.df.columns[0]][i] == clave:
+            for i in range(len(self.data_dict[self.nombre_columnas[0]])):
+
+                if list(self.data_dict[self.nombre_columnas[0]][i])[0] == clave:
                     index = i
                     return index
 
 
-            print(f"Error: La clave '{clave}' no existe en el archivo '{self.path}'")
+            print(f"Error: La clave '{clave}' no existe en la DB '{self.db}'")
             return None
 
     def obtener_valor(self,columna, clave):
         if self.obtener_index_fila(clave) != None and self.obtener_index_columna(columna) != None:
-            return self.diccionario[columna][self.obtener_index_fila(clave)]
+            return list(self.data_dict[columna][self.obtener_index_fila(clave)])[0]
         
         else:
             print(f"Error: La columna '{columna}' y clave '{clave}' no existen en el archivo '{self.path}'")
             return None
 
     def obtener_valores_columna(self, columna):
-        if columna not in self.diccionario:
-            print(f"Error: La columna '{columna}' no existe en el archivo '{self.path}'")
+        if columna not in self.data_dict:
+            print(f"Error: La columna '{columna}' no existe en la DB '{self.db}'")
             return None
 
         output = []
-        # Se empieza a iterar en 1 para eliminar el primer valor de la lista, 
-        # que contiene el nombre de la columna
-        # no se usa el método pop() porque el primer elemento es `nan`
-        for i in range(1, len(self.diccionario[columna])):
-            output.append(self.diccionario[columna][i])
+        for i in self.data_dict[columna]:
+            if list(i) != [''] :
+                output.append(list(i)[0])
         return output
 
     def obtener_valores_fila(self, fila):
         output = []
  
-        for columna in self.df.columns:
-            output.append(self.diccionario[columna][self.obtener_index_fila(fila)])
-
+        for columna in self.nombre_columnas:
+            if list(self.data_dict[columna][self.obtener_index_fila(fila)])[0] != '':
+                output.append(list(self.data_dict[columna][self.obtener_index_fila(fila)])[0])
         # Se elimina el primer elemento, porque contiene el nombre de la fila
         output.pop(0)
         return output
 
 # el archivo .xlsx que contiene las respuestas, está en el mismo directorio
 # que este archivo actions.py
-data = manager_dataDB("dataDB.xlsx")
+data = manager_dataDB("data", "dataDB.sqlite")
 
 # esto esrá para evitar que el chatbot responda dos veces 
 was_submitted = False
@@ -148,6 +178,15 @@ class ValidateAsignaturaTipoDatoForm(FormValidationAction):
                 slot_value_purged.append(i)
 
         print("[DEBUG] slot_value_purged: ", slot_value_purged)
+        tmp1 = []
+        for i in range(len(slot_value_purged)):
+            if slot_value_purged[i] not in ALLOWED_SUBJECTS: 
+                j = slot_value_purged[i].split()
+                for k in j:
+                    if k in ALLOWED_SUBJECTS:
+                        tmp1.append(k)
+            if slot_value_purged[i] in ALLOWED_SUBJECTS:
+                tmp1.append(slot_value_purged[i])
 
         # obtener las asignaturas que pidió el usuario obtenido su correspondencia a las ALLOWED
         for asignat in slot_value_purged:
@@ -164,7 +203,7 @@ class ValidateAsignaturaTipoDatoForm(FormValidationAction):
 
         # Caso en el que la asignatura dada no esté en la DB
         if in_Input == False:
-            dispatcher.utter_message(text=f"De momento solo puedo informarle sobre las siguientes asignaturas: {subjects}")
+            #dispatcher.utter_message(text=f"De momento solo puedo informarle sobre las siguientes asignaturas: {subjects}")
             return {"asignatura": None}
         
         # Para comprobar si ya se tiene el tipo_dato, en caso negativo se pide, en caso positivo se muestra la info solicitada
@@ -224,7 +263,8 @@ class ValidateAsignaturaTipoDatoForm(FormValidationAction):
         # Comprobar si todavía no se ha dado el valor de la asignatura
         slot_asignatura = tracker.get_slot("asignatura")
         if slot_asignatura == None:
-            dispatcher.utter_message(text=f"Ahora necesito que usted me diga la asignatura que le interesa de estas: {subs}")
+            #dispatcher.utter_message(text=f"Ahora necesito que usted me diga la asignatura que le interesa de estas: {subs}")
+            pass
         else:
             # eliminar duplicados en asignatura
             slot_value_purged = []
